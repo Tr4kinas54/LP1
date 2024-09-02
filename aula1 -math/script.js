d99352cc300ae4afd3f887cad97280018696291f
DePalmatorias <Trakinas2810@gmail.com>
2024-09-02 08:26:25 -0300

    Aula-Math

---
+++ aula1 -math/script.js
@@ -0,0 +1,26 @@
+let numero = 4.7
+
+//arredondamento comum
+console.log(Math.round(numero))
+
+//arredonda pra cima
+console.log(Math.ceil(numero))
+
+//arredonda pra baixo
+console.log(Math.floor(numero))
+
+//math.pow gera potenciacao
+console.log(Math.pow(5, 2))
+
+//math.sqrt gera raiz quadrada
+console.log(Math.sqrt(35))
+
+//math.abs gera um numero absoluto positivo
+console.log(Math.abs(-5))
+
+//math.min e math.max calculam o maior e o menor numero
+console.log(Math.min(7,45,89,2,125))
+console.log(Math.max(7,45,89,2,125))
+
+let numeroAleatorio = Math.floor(Math.random() * 11)  
+console.log(numeroAleatorio)
