44e446ed10371ec4545a12ba6aacf04cf7f0acab
DePalmatorias <Trakinas2810@gmail.com>
2024-09-02 08:44:59 -0300

    exercicio 1 e 2

---
+++ aula1 -math/script2.js
@@ -0,0 +1,5 @@
+let numero1 = parseFloat(prompt('Digite um numero'))
+let numero2 = parseFloat(prompt('Digite outro numero'))
+let media = (numero1 + numero2) / 2
+
+console.log('A media é' + Math.round(media))
