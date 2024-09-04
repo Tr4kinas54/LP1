ad2526d9aeab798791fb7fecf56ead826a5deae1
Tr4kinas54 <Trakinas2810@gmail.com>
2024-09-04 13:00:18 -0300

    potencia

--- aula1 -math/script3.js
+++ aula1 -math/script3.js
@@ -1,4 +1,4 @@
 let umero = parseInt(prompt('Digite um numreo para que seja elevado'))
 let potencia = parseInt(prompt('Digite um numero para que seja a potencia'))
 
-console.log(`Resultafo: ${Math.pow(umero, potencia)}`)
+console.log(`Resultafo: ${Math.pow(umero, potencia)}`)dsfb
