0e9e2fc91e13012cd5c51494dc839ce04934e870
Mauricio-Assis <167426483+AceSxxx@users.noreply.github.com>
2024-09-16 08:38:27 -0300

    numeroaleatorio

--- Aula2/script.js
+++ Aula2/script.js
@@ -4,12 +4,21 @@ function gerarnumeroaleatorio(){
 }
 
 function jogar(){
-    const peganumeroaleatorio = gerarnumeroaleatorio()
-    console.log(peganumeroaleatorio)
+    const numero = gerarnumeroaleatorio()
+    let palpite = ""  
+    let tentaivas = 3   
     do{
-        const palpite = prompt("Adivinhe o Número")
-    }
-    while()
+        console.log(numero)
+        palpite = parseInt(prompt('Adivinhe o número'))
+        if(numero != palpite){
+            tentaivas--
+            alert(`Ererou ! genta de nivo! Voced ainda tem ${tentaivas > 1? "tentativas": "tentaticva"}.`)
+        }else{
+            alert(`ParabenS  O NUMER O ERA :${numero}`)
+            return
+        }
+    }while(tentaivas > 0)
+        alert(`Parabens ! o NUmer jeran: ${numero}`)
+    
 }
-
 jogar()
